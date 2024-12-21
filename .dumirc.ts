bc1f46c4c9ad57b4c23c5ad7ec7cf97c555e8272
xuguanjie <215790428@qq.com>
2024-12-22 00:13:32 +0800

    fix

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   outputPath: 'docs-dist',
-  base:"an-mobile",
+  base:"/an-mobile",
   themeConfig: {
     name: 'an-mobile',
     // deviceWidth: 375,
