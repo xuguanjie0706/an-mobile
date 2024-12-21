b5218e5456d526a23d433e78047ade4576c8e099
xuguanjie <215790428@qq.com>
2024-12-21 22:49:40 +0800

    修改打包路径

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   outputPath: 'docs-dist',
+  base:"an-mobile",
   themeConfig: {
     name: 'an-mobile',
     // deviceWidth: 375,
