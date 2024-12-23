356af6e5f653f919e04d82ad096ba48262d8cce1
xuguanjie <215790428@qq.com>
2024-12-23 10:36:48 +0800

    build: 修改basebase

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   outputPath: 'docs-dist',
-  publicPath:"/an-mobile/",
+  publicPath: '/an-mobile/',
+  base: '/an-mobile/',
   themeConfig: {
     name: 'an-mobile',
     logo: false,
